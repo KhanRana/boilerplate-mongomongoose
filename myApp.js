f6f358b323f322af57f2c398f61166e96f6d419f
KhanRana <47982799+KhanRana@users.noreply.github.com>
2024-02-12 11:31:26 +0000

    add link to the database

--- myApp.js
+++ myApp.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
+const mongoose = require("mongoose")
 
+//Connect to the database
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
